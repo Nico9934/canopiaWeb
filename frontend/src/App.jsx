bd9860e5ea2f5a30a3eb7e2e2a58e561fb513048
nico9934 <nico9934@gmail.com>
2024-02-09 15:27:56 -0300

    Error router intent fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import ClientesProvider from "./components/UserContext";
 import Header from "./components/Header";
